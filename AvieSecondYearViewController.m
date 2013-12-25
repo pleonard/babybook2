55a235fec467c5020f398d847de02b3153c55dde
Patrick Leonard <pleonard@healthagen.net>
2013-12-25 06:55:12 -0700

    text changes to fit better

--- AvieSecondYearViewController.m
+++ AvieSecondYearViewController.m
@@ -44,7 +44,7 @@
     _avieImages = @[@"mar_first_birthday.jpg", @"apr_walking_park.jpg", @"may_comedy.jpg", @"jun_marigolds.jpg", @"jul_ellie_birthday.jpg", @"aug_dillon_hiking.jpg", @"sep_ducks.jpg", @"oct_waving.jpg", @"nov_pigtails.jpg", @"dec_snowball.jpg"
     ];
     _avieDescriptions = @[
-        @"Mommy threw Avie an amazing first birthday party full of family and friends and with the theme of Hungry Little Caterpillar. When it came time for cake, our little health nut tried one bite then went back to her pasta and veggies.",
+        @"Mommy threw Avie an amazing first birthday party full of family and friends. When it came time for cake, our little health nut tried one bite then went back to her pasta and veggies.",
         @"Avie loves being outside so much that as soon as she started walking on her own she took off across the park. We were so surprised that she was 30 yards away before Mommy started chasing after.",
         @"Early on Avie figured out that she is hilarious and she loves cracking Mommy and Daddy up. After the sunglasses came the famous 'Smooshyface'.",
         @"Marigolds, beware! Avie loves the smell of marigolds and spent the whole summer picking and smelling them. Good thing they're cheap and we grew a lot of them.",
@@ -52,7 +52,7 @@
         @"One of Avie's early words was 'hiking'. We spent a lot of time in the mountains this summer, hiking with Avie and at the condo in Dillon. She loves hiking and being outdoors.",
         @"Mommy and Avie spent many summer days walking to the lake in Wash Park to feed the ducks. Avie loves feeding the ducks in Wash Park. Daddy got to go sometimes too, what a great way to spend the day.",
         @"Leaving Avie to go to work every morning is hard but watching her wave goodbye is the best send off ever. Even when it's cold out, she comes outside to wave. It's just the best.",
-        @"It's exciting to see Avie growing and learning so fast, but sometimes it feels a little too fast. As she grows to become the amazing person we know she is going to be, Mommy and Daddy will always think of Avie as the sweet, smart, funny little girl in pigtails.",
+        @"It's exciting to see Avie growing and learning so fast, but sometimes it feels a little too fast. Mommy and Daddy will always think of Avie as the sweet, smart, funny little girl in pigtails.",
         @"We didn't get much snow in December but Avie quickly learned to make and throw snowballs and to 'hop in the snow'. She looks out the window every morning to see if there is snow."
     ];
 
